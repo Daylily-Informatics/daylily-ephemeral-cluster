4bcf15d7aca478ceae2f0acd3be984acd2c40630
John Major <daylily@daylilyinformatics.com>
2025-10-13 22:04:37 -0700

    X

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if SCRIPTS_DIR.exists():
 
 setup(
     name="daylily-ephemeral-cluster",
-    version="0.7.360",
+    version="0.7.361",
     packages=find_packages(),
     include_package_data=True,
     scripts=script_files,
