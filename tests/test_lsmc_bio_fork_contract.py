47a71ea73e794a77e50e3c781130838c9d1acb8c
John Major <iamh2o@gmail.com>
2026-06-12 08:08:51 -0700

    Pin DayOA 10.0.7 in DYEC catalog

--- tests/test_lsmc_bio_fork_contract.py
+++ tests/test_lsmc_bio_fork_contract.py
@@ -8,8 +8,8 @@ import yaml
 REPO_ROOT = Path(__file__).resolve().parents[1]
 
 _OLD_ORG = "Daylily-" + "Informatics"
-DAYOA_BLESSED_TAG = "10.0.6"
-DYEC_BLESSED_TAG = "10.0.15"
+DAYOA_BLESSED_TAG = "10.0.7"
+DYEC_BLESSED_TAG = "10.0.16"
 
 FORBIDDEN_ACTIVE_REFERENCES = (
     f"{_OLD_ORG}/daylily-omics-analysis",
