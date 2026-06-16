226f91642be488434fce8926c5ba899bdb4843e7
John Major <iamh2o@gmail.com>
2026-06-15 17:38:53 -0700

    Bump DYEC self pin to 10.0.39

--- tests/test_lsmc_bio_fork_contract.py
+++ tests/test_lsmc_bio_fork_contract.py
@@ -9,7 +9,7 @@ REPO_ROOT = Path(__file__).resolve().parents[1]
 
 _OLD_ORG = "Daylily-" + "Informatics"
 DAYOA_BLESSED_TAG = "10.0.23"
-DYEC_BLESSED_TAG = "10.0.37"
+DYEC_BLESSED_TAG = "10.0.39"
 
 FORBIDDEN_ACTIVE_REFERENCES = (
     f"{_OLD_ORG}/daylily-omics-analysis",
