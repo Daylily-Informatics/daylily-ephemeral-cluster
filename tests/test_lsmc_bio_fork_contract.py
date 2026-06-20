272d89ce4c5f3ce1a874cbc1ea98f31a10be649e
John Major <iamh2o@gmail.com>
2026-06-19 19:18:48 -0700

    Pin DayOA 10.0.34

--- tests/test_lsmc_bio_fork_contract.py
+++ tests/test_lsmc_bio_fork_contract.py
@@ -8,7 +8,7 @@ import yaml
 REPO_ROOT = Path(__file__).resolve().parents[1]
 
 _OLD_ORG = "Daylily-" + "Informatics"
-DAYOA_BLESSED_TAG = "10.0.33"
+DAYOA_BLESSED_TAG = "10.0.34"
 DYEC_BLESSED_TAG = "10.0.58"
 
 FORBIDDEN_ACTIVE_REFERENCES = (
