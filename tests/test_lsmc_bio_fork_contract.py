75b8273bfc6396e6decb6de7c891f5260a2df460
John Major <iamh2o@gmail.com>
2026-07-09 14:20:41 -0700

    Self-pin DYEC 10.0.127

--- tests/test_lsmc_bio_fork_contract.py
+++ tests/test_lsmc_bio_fork_contract.py
@@ -9,7 +9,7 @@ REPO_ROOT = Path(__file__).resolve().parents[1]
 
 _OLD_ORG = "Daylily-" + "Informatics"
 DAYOA_BLESSED_TAG = "10.0.75"
-DYEC_BLESSED_TAG = "10.0.125"
+DYEC_BLESSED_TAG = "10.0.127"
 
 FORBIDDEN_ACTIVE_REFERENCES = (
     f"{_OLD_ORG}/daylily-omics-analysis",
