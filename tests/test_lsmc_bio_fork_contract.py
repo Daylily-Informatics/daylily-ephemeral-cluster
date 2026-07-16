a9749a80e1bb7475a0b29575f2e25c99b0eead1f
John Major <iamh2o@gmail.com>
2026-07-15 17:07:57 -0700

    Pin Sentieon catalogs to DayOA 11.0.9

--- tests/test_lsmc_bio_fork_contract.py
+++ tests/test_lsmc_bio_fork_contract.py
@@ -8,7 +8,7 @@ import yaml
 REPO_ROOT = Path(__file__).resolve().parents[1]
 
 _OLD_ORG = "Daylily-" + "Informatics"
-DAYOA_DEFAULT_TAG = "11.0.8"
+DAYOA_DEFAULT_TAG = "11.0.9"
 DAYOA_VALIDATED_TAG = "11.0.7"
 DYEC_BLESSED_TAG = "10.3.14"
 
