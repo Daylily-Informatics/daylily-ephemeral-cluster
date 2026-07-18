bada24e3641bc0760298226464def28ca2b3d6da
John Major <iamh2o@gmail.com>
2026-07-17 22:58:41 -0700

    Advance DYEC self-pin to 11.0.1

--- tests/test_lsmc_bio_fork_contract.py
+++ tests/test_lsmc_bio_fork_contract.py
@@ -10,7 +10,7 @@ REPO_ROOT = Path(__file__).resolve().parents[1]
 _OLD_ORG = "Daylily-" + "Informatics"
 DAYOA_DEFAULT_TAG = "12.0.1"
 DAYOA_VALIDATED_TAG = "12.0.1"
-DYEC_BLESSED_TAG = "11.0.0"
+DYEC_BLESSED_TAG = "11.0.1"
 
 FORBIDDEN_ACTIVE_REFERENCES = (
     f"{_OLD_ORG}/daylily-omics-analysis",
