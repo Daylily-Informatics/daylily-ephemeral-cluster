ab4118969c4d4918fd89939e43ce7fc521ea5b47
John Major <iamh2o@gmail.com>
2026-07-18 03:30:32 -0700

    Pin DYEC release 11.0.5

--- tests/test_lsmc_bio_fork_contract.py
+++ tests/test_lsmc_bio_fork_contract.py
@@ -10,7 +10,7 @@ REPO_ROOT = Path(__file__).resolve().parents[1]
 _OLD_ORG = "Daylily-" + "Informatics"
 DAYOA_DEFAULT_TAG = "12.0.3"
 DAYOA_VALIDATED_TAG = "12.0.2"
-DYEC_BLESSED_TAG = "11.0.3"
+DYEC_BLESSED_TAG = "11.0.5"
 
 FORBIDDEN_ACTIVE_REFERENCES = (
     f"{_OLD_ORG}/daylily-omics-analysis",
