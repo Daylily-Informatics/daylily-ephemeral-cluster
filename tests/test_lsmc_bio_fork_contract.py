8c576b28c8d7bd9a6d52d6784fed387ca014a65c
John Major <iamh2o@gmail.com>
2026-07-23 19:19:58 -0700

    Update DYEC self pin to 14.0.8

--- tests/test_lsmc_bio_fork_contract.py
+++ tests/test_lsmc_bio_fork_contract.py
@@ -10,7 +10,7 @@ REPO_ROOT = Path(__file__).resolve().parents[1]
 _OLD_ORG = "Daylily-" + "Informatics"
 DAYOA_DEFAULT_TAG = "13.0.37"
 DAYOA_VALIDATED_TAG = "13.0.37"
-DYEC_BLESSED_TAG = "14.0.5"
+DYEC_BLESSED_TAG = "14.0.8"
 
 FORBIDDEN_ACTIVE_REFERENCES = (
     f"{_OLD_ORG}/daylily-omics-analysis",
