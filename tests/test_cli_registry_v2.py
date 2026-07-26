bbab3fd289921ce8039a9a24c5ba86fe9de61778
John Major <iamh2o@gmail.com>
2026-07-26 10:10:09 -0700

    Pin DYEC catalog to DayOA 13.0.46

--- tests/test_cli_registry_v2.py
+++ tests/test_cli_registry_v2.py
@@ -27,7 +27,7 @@ from daylily_ec.state.models import StateRecord
 runner = CliRunner()
 
 
-DAYOA_BLESSED_TAG = "13.0.44"
+DAYOA_BLESSED_TAG = "13.0.46"
 
 EXPECTED_COMMANDS = {
     ("version",),
--- tests/test_tests_runner.py
+++ tests/test_tests_runner.py
@@ -55,7 +55,7 @@ from daylily_ec.tests_runner import (
 
 
 runner = CliRunner()
-DAYOA_BLESSED_TAG = "13.0.44"
+DAYOA_BLESSED_TAG = "13.0.46"
 
 
 def _run_mount_record(
